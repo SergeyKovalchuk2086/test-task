8bf85bd946be69da9f14e41c87d5088a755aaf15
SergeyKovalchuk2086 <kovalchuk.s.v@inbox.ru>
2021-09-24 18:38:18 +0700

    add some js

--- script/script.js
+++ script/script.js
@@ -1,58 +1,127 @@
 const fileUploader = document.getElementById('file');
-
-fileUploader.addEventListener('change', (event) => {
-	const files = event.target.files;
-	console.log('files', files);
-
-	for (const file of files) {
-		let name = file.name;
-		let type = file.type;
-		const size = file.size;
-
-
-		console.log(name);
-		console.log(type);
-
-		let format = type.split('/').pop().toUpperCase();
-		console.log(format);
-
-		name = name.slice(0, name.indexOf("."));
-		const block = document.getElementById('documentInfo');
-
-		let blockInfo = `
-			<div class="info">
-				<span class="fileName">${name}</span>
-				<span class="fileSize">${format + ' ' + (size / 1024 / 1014).toFixed(3) + ' mb'}</span>
-			</div>
-			<img src="./img/basket.png" alt="basket" class="basket">
-			`
-		block.innerHTML = blockInfo;
-
-	}
-});
-
 const reader = new FileReader();
 const imageGrid = document.getElementById('documentInfo');
 
-fileUploader.addEventListener('change', (event) => {
-	const files = event.target.files;
-	const file = files[0];
-	reader.readAsDataURL(file);
+const name = document.getElementById('name');
+const select = document.getElementById('select');
+const country = document.getElementById('country');
+const city = document.getElementById('city');
+const date = document.getElementById('date');
+console.log(date);
 
-	reader.addEventListener('load', (event) => {
-		const img = document.createElement('img');
-		imageGrid.prepend(img);
-		img.src = event.target.result;
-		img.alt = file.name;
-	});
-});
+const secondList = document.getElementById('secondList');
 
-fileUploader.addEventListener('click', function () {
-	let div = document.getElementById('documentInfo');
 
-	if (div.style.display == 'none') {
-		div.style.display = 'flex';
-	} else {
-		div.style.display = 'flex';
+let proverka = {
+	inputName: false,
+	inputSelect: '',
+	inputCountry: false,
+	inputCity: false,
+}
+
+//inputName
+name.addEventListener('keyup', function (event) {
+	if (event.target.value.length >= 1) {
+		proverka.inputName = true;
+		name.style.borderColor = "#8E43ED";
 	}
-});
+})
+
+//inputSelect
+select.addEventListener('change', function (event) {
+	if (event.target.value == "Male") {
+		proverka.inputSelect = "Male";
+		select.style.color = "black";
+	} else if (event.target.value == "Female") {
+		proverka.inputSelect = "Female";
+		select.style.color = "black";
+	}
+	select.style.borderColor = "#8E43ED";
+
+	console.log(proverka);
+	if ((proverka.inputName == true) && (proverka.inputSelect == "Male" || proverka.inputSelect == "Female")) {
+		secondList.style.display = 'block';
+	}
+})
+
+
+//inputCountry
+country.addEventListener('keyup', function (event) {
+	if (event.target.value.length >= 1) {
+		proverka.inputCountry = true;
+		country.style.borderColor = "#8E43ED";
+	}
+})
+
+
+//inputCity
+city.addEventListener('keyup', function (event) {
+	if (event.target.value.length >= 1) {
+		proverka.inputCity = true;
+		city.style.borderColor = "#8E43ED";
+	}
+})
+
+//inputDate
+date.addEventListener('click', function (event) {
+	// if (date.value !== '' ) {
+	// 	proverka.inputDate = true;
+	// 	date.style.borderColor = "#8E43ED";
+	// }
+
+	console.log(event.taget);
+})
+
+//добавляем блок с информацией о файле
+// fileUploader.addEventListener('change', (event) => {
+// 	const files = event.target.files;
+// 	console.log('files', files);
+
+// 	for (const file of files) {
+// 		let name = file.name;
+// 		let type = file.type;
+// 		const size = file.size;
+
+// 		let format = type.split('/').pop().toUpperCase();
+// 		console.log(format);
+
+// 		name = name.slice(0, name.indexOf("."));
+// 		const block = document.getElementById('documentInfo');
+
+// 		let blockInfo = `
+// 			<div class="info" id="info">
+// 				<span class="fileName">${name}</span>
+// 				<span class="fileSize">${format + ' ' + (size / 1024 / 1014).toFixed(3) + ' mb'}</span>
+// 			</div>
+// 			<img src="./img/basket.png" alt="basket" class="basket" id="basket">
+// 			`
+// 		block.innerHTML = blockInfo;
+// 	}
+
+// 	const file = files[0];
+// 	reader.readAsDataURL(file);
+
+// 	reader.addEventListener('load', (event) => {
+// 		const img = document.createElement('img');
+// 		imageGrid.prepend(img);
+// 		img.src = event.target.result;
+// 		img.alt = file.name;
+// 	});
+// });
+
+//если файл загружен меняем стиль для кнопки
+// fileUploader.addEventListener('click', function () {
+// 	let div = document.getElementById('documentInfo');
+// 	let sendBtn = document.getElementById('sendBtn');
+
+// 	if (div.style.display == 'none') {
+// 		div.style.display = 'flex';
+// 	} else {
+// 		div.style.display = 'flex';
+// 	}
+
+// 	if (div.style.display = 'flex') {
+// 		sendBtn.style.background = "#8e43ed";
+// 	}
+// });
+
